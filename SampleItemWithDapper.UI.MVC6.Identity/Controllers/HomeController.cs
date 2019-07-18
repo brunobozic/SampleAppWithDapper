2e7269525497193726a920534ce8a161b3fd64e6
brunobozic <bruno.bozic@gmail.com>
2019-07-18 13:56:46 +0200

    Migration to MVC 6
    Migration to MVC 6

---
+++ SampleItemWithDapper.UI.MVC6.Identity/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleItemWithDapper.UI.MVC6.Identity.Models;
+
+namespace SampleItemWithDapper.UI.MVC6.Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleItemWithDapper.UI.MVC6.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleItemWithDapper.UI.MVC6.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
