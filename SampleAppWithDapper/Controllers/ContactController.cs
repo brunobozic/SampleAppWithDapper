f3817d179474a435c743dab062c828430022075b
brunobozic <bruno.bozic@gmail.com>
2019-07-19 00:10:09 +0200

    Added masked input for phone input fields.
    Added masked input for phone input fields.

--- SampleAppWithDapper/Controllers/ContactController.cs
+++ SampleAppWithDapper/Controllers/ContactController.cs
@@ -247,8 +247,6 @@ namespace SampleAppWithDapper.Controllers
                                 "<a type='button' class='btn btn-light btnGridEdit' style='padding:6px; font-size: 12px !important' href='" +
                                 this.Url.Action("Edit", "Contact", new {Id = item.Id}) +
                                 "'><i class= 'fa fa-edit fa-lg'></i> EDIT</a>";
-                            //item.Created = item.CreatedUtc.LocalDateTime;
-                            //item.Modified = item.ModifiedUtc?.LocalDateTime;
                         }
                     }
 
